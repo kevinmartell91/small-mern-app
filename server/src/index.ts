ba09d2b36786cd08a7d3b6649ca1651a7a3c815c
kevinmartel91 <kevinmartell91@gmail.com>
2022-06-14 21:32:12 -0500

    Added mock data

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,34 @@
-import express from "express";
+import express from 'express';
+import bodyParser from 'body-parser';
+import { listings } from './listings';
+
 const app = express();
 const port = 9000;
+app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello words");
+app.get('/', (_req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.send('Hello words');
 });
 
-app.listen(port);
+app.get('/mock-listing', (_req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.send(listings);
+});
 
-console.log(`[app]: http://localhost:${port}`);
+app.post('/delete-listing/:id', (req, res) => {
+  const id: string = req.body.id;
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  for (let i = 0; i < listings.length; i++) {
+    if (listings[i].id === id) {
+      return res.send(listings.splice(i, 1));
+    }
+  }
+});
+
+app.listen(port, function () {
+  console.log(`[app]: http://localhost:${port}/`);
+});
